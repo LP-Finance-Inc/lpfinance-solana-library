188d5c91f4eebab85a7d621ab486ccba5e4d1ffd
Deepakkurmi-eMoment <deepak.kurmi@emoment.in>
2022-06-17 16:20:25 +0530

    first commit

---
+++ src/calculator.js
@@ -0,0 +1,15 @@
+export const Addition = (num1, num2) => {
+  return num1 + num2;
+};
+
+export const Subtract = (num1, num2) => {
+  return num1 - num2;
+};
+
+export const Multiplay = (num1, num2) => {
+  return num1 * num2;
+};
+
+export const Division = (num1, num2) => {
+  return num1 / num2;
+};
