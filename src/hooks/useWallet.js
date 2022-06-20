d2cf28db869e98793789b48ab4646c01b00d81ff
Deepakkurmi-eMoment <deepak.kurmi@emoment.in>
2022-06-20 22:49:55 +0530

    create a function for cbs

--- src/hooks/useWallet.js
+++ src/hooks/useWallet.js
@@ -1,9 +1,9 @@
-import { Wallet } from "@project-serum/anchor";
-import bs58 from "bs58";
 const { Keypair } = require("@solana/web3.js");
 
 export const useWallet = (PRIVATE_KEY) => {
-  const wallet = new Wallet(Keypair.fromSecretKey(bs58.decode(PRIVATE_KEY)));
+  var secretKey = Uint8Array.from(PRIVATE_KEY);
+  var wallet = Keypair.fromSecretKey(secretKey);
+
   return {
     wallet,
   };
